8b60c38d841ad147270318a4a982d820ba2f1d84
An-another-Artem <an.another.artem@gmail.com>
2024-05-24 15:38:19 +0300

    Create ServeCommand.go

---
+++ ServeCommand.go
@@ -0,0 +1,17 @@
+package proxy_shell
+
+import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ServeCommand(group fiber.Group, command Cmd) error {
+	group.Get("/", func(ctx *fiber.Ctx) error {
+		buffer, err := json.Marshal(command)
+		if err != nil {
+			return ctx.Status(503).JSON(buffer)
+		}
+		return ctx.Status(200).JSON(buffer)
+	})
+	return nil
+}
