2eea4106a1bce341d51efbf6c7fd719013c7bf09
An-another-Artem <an.another.artem@gmail.com>
2024-05-24 15:31:44 +0300

    Create Execute.go

---
+++ Execute.go
@@ -0,0 +1,22 @@
+package proxy_shell
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func ExecCommand(commands string) error {
+	commandAndArgs := strings.Split(commands, " ")
+	cmd := exec.Command(commandAndArgs[1], strings.Join(commandAndArgs[2:], " "))
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	defer func(cmd *exec.Cmd) {
+		err := cmd.Run()
+		if err != nil {
+			return
+		}
+	}(cmd)
+	return nil
+}
