865b8abfa13cb8aedeadcd55ba5feac3fbc30f36
An-another-Artem <an.another.artem@gmail.com>
2024-05-27 18:39:59 +0300

    Update CreateLog.go

--- CreateLog.go
+++ CreateLog.go
@@ -11,7 +11,7 @@ func CreateLog(file *os.File) (*zerolog.Logger, error) {
 	if file != nil {
 		output := zerolog.ConsoleWriter{Out: file, TimeFormat: time.RFC850}
 		output.FormatLevel = func(i interface{}) string {
-			return fmt.Sprintf("| Level: %s | proxy-shell 1.0 |", i)
+			return fmt.Sprintf("| Level: %s | thanks for using this good (actually bad) software! |", i)
 		}
 		logger := zerolog.New(output)
 		return &logger, nil
