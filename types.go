3e0e2a44263362e7c0b80004a91842cb12be7688
An-another-Artem <an.another.artem@gmail.com>
2024-05-29 18:01:17 +0300

    Update types.go

--- types.go
+++ types.go
@@ -1,63 +1,9 @@
 package proxy_shell
 
-import (
-	"encoding/json"
-	"github.com/gofiber/fiber/v2"
-	"github.com/rs/zerolog"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
-	"sync"
-)
-
 type Cmd string
 
 type Config struct {
 	LocalURL  string `json:"localURL"`
 	RemoteURL string `json:"remoteURL"`
 	Version   string `json:"version"`
-}
-type ProxyRequest struct {
-	Command Cmd
-	SendTo  url.URL
-}
-type LogRequest struct {
-	Message   string
-	Level     zerolog.Level
-	BoolName  string
-	BoolValue bool
-}
-type Client struct {
-	Url         url.URL
-	ConnectedTo string
-	Mutex       sync.Mutex
-	Alive       bool
-}
-
-func (request ProxyRequest) RunProxyRequest(w *http.ResponseWriter, r *http.Request, command Cmd, group *fiber.Router, app *fiber.App) error {
-	request.Command = command
-	if group != nil {
-		err := ServeCommand(app, *group, request.Command)
-		if err != nil {
-			return err
-		}
-	}
-	if w != nil && r != nil {
-		proxy := httputil.NewSingleHostReverseProxy(&request.SendTo)
-		proxy.ServeHTTP(*w, r)
-	}
-	return nil
-}
-func (rawCfg Config) CreateConfig(name string, dir string) error {
-	err := os.Chdir(dir)
-	if err != nil {
-		return err
-	}
-	data, err := json.Marshal(rawCfg)
-	err = os.WriteFile(name+".psCfg", data, 0666)
-	if err != nil {
-		return err
-	}
-	return nil
 }
