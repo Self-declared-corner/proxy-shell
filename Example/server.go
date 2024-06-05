097d361604d45705839f364f8e6284a0f436685b
An-another-Artem <an.another.artem@gmail.com>
2024-06-05 09:31:32 +0300

    Create server.go

---
+++ Example/server.go
@@ -0,0 +1,12 @@
+// 2024 The Corner. This software is using GPL-3.0 licence. Licence can be found in the LICENCE file, one directory up.
+
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{DisableStartupMessage: true})
+
+}
